4fee130ff9d8fd50f5dfe414d90e9408430830d1
ramin rowshan <raminrowshan153@gmail.com>
2021-12-18 19:15:01 +0330

    adding header

---
+++ lexheader.h
@@ -0,0 +1,39 @@
+#define OPEN_BRACKET 1
+#define CLOSE_BRACKET 2
+#define OPEN_PARANTEZ 3
+#define CLOSE_PARANTEZ 4
+#define COLONS 5
+
+        // types
+#define INTEGER 6
+#define FLOAT 7
+#define STRING 8
+
+		//operator
+#define RELOPE 9
+#define ASSIGN 10
+#define SUM 11
+#define DIVIDE 12
+#define MULTIPLY 13
+#define SUBTRACT 14
+#define REMINDER 15
+
+        // conditions
+#define IF 16
+#define ELSE 17
+#define ELIF 18
+#define AND 19
+#define OR 20
+#define NOT 21
+#define WHILE 22
+        // loops
+#define FOR 23
+#define BREAK 24
+#define CONTINUE 25
+
+#define VARIABLE 26
+
+		// new_line
+#define NEW_LINE 27
+
+#define NONE_TYPE 28
